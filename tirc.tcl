33239b181aebc7391b5d264208e1cac56c3f9503
Edward Brekelbaum <nedbrek@yahoo.com>
2010-08-25 07:25:39 -0500

    Nearly functional client    Added wait for PING before JOIN    Improved PING pattern match    Reworked PING debug output    Added parser for normal messages (encoded as PRIVMSG)    Added scrollbar

--- tirc.tcl
+++ tirc.tcl
@@ -1,5 +1,6 @@
 set server "BROOKVILLE.PA.US.StarLink-IRC.Org"
 set chn #prosapologian
+set ::gotPing 0
 
 proc send {msg} {
 	puts $::net $msg
@@ -26,28 +27,53 @@ proc recv {} {
 	}
 
 	gets $::net line
-	if [regexp {^PING (:[0-9]+)} $line -> code] {
+	if [regexp {^PING (:[0-9A-Za-z.\-]+)} $line -> code] {
 		send "PONG $code"
 
-		.t.txt insert end "PING\n"; .t.txt yview end
+		if {!$::gotPing} {
+			send "JOIN $::chn"
+			set ::gotPing 1
+		}
+
+		.t.txt insert end "$line\n"
+		.t.txt insert end "PONG $code\n";
 		return
 	}
 
-	.t.txt insert end "$line\n"
-	.t.txt yview end
+	if {[regexp {[^ ]+ +PRIVMSG } $line]} {
+		set cols [split $line]
+
+		set sender [lindex $cols 0]
+		set bangIdx [string first "!" $sender]
+		set sendName [string range $sender 1 $bangIdx]
+
+		set first [lindex $cols 3]
+		.t.txt insert end "$sendName [string range $first 1 end] "
+		.t.txt insert end "[lrange $cols 4 end]\n"
+	} else {
+		.t.txt insert end "$line\n"
+		.t.txt yview end
+	}
 }
 
 wm withdraw .
 toplevel .t
 
-pack [text  .t.txt] -expand 1 -fill both
+pack [frame .t.fTop] -side top -fill both -expand 1
+
+pack [scrollbar .t.fTop.scrollV -orient vert -command ".t.txt yview"
+] -side right -expand 1 -fill y
+
+pack [text  .t.txt -yscrollcommand ".t.fTop.scrollV set"
+] -expand 1 -fill both -in .t.fTop -side left
 pack [entry .t.cmd] -expand 1 -fill x
 bind .t.cmd <Return> post
-
+ 
+set ::gotPing 0
 set net [socket $server 6667]
+
 fileevent $net readable recv
 
 send "NICK NedTclTest"
 send "USER ned ned ned :NedBrek"
-send "JOIN $chn"
 
